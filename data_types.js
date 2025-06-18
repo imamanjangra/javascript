b0fd5ac839347293b5c10ce129408273fce93b86
imamanjangra <imamanjangra@email.com>
2025-06-18 14:35:38 +0530

    data types and conversion

---
+++ data_types.js
@@ -0,0 +1,45 @@
+//types of data types
+let num = 15; // Number 
+let str = "Aman" // string
+let value = true; // Boolean -> true or false
+let data ; // undefined 
+let sum = null; //null
+
+console.table([typeof(num) , typeof(str) , typeof(value) , typeof(data) , typeof(sum)]);
+// _______________________________________________________________________________________
+// Datatype conversion 
+
+//convert into number
+
+// 1) string -> number
+change_data = Number(sum);
+console.log(typeof(change_data));
+console.log(change_data);// value is not a number NaN
+
+//2) boolean -> number
+//true = 1
+//fale = 0
+
+//3) null ->  number
+// value = 0
+
+//4) undefined ->  number
+// value = NaN
+// ____________________________
+
+// convert into string
+
+//num -> string
+let change_data = String(value);
+console.log(typeof(change_data));
+console.log(change_data);
+
+//null -> string
+// value = null
+
+//undefined -> string
+// value = undefined
+
+//boolean -> string
+// value = true or false
+
