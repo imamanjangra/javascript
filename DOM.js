b8bc4775786d22b266af68407d7eb7d7b0fa9228
imamanjangra <imamanjangra@email.com>
2025-07-08 16:53:20 +0530

    DOM

--- DOM.js
+++ DOM.js
@@ -2,12 +2,16 @@
 // console.log(div.innerText + " -> innerText");// not show hidden text 
 // console.log(div.textContent + " -> textcontent");// show hidden text 
 
+// const { createElement } = require("react");
+
+// const { createElement } = require("react");
+
 
 // const myul = document.querySelector("ul");
 // const myli = myul.querySelector("li");
 // myli.style.backgroundColor = "Green";
 
-//querySelectorAll -> foreach
+// querySelectorAll -> foreach
 
 // let list = document.querySelectorAll("li");
 
@@ -22,41 +26,71 @@
 // arr.forEach((l) => {l.style.backgroundColor = "red"})
 
 
-let prnt = document.querySelector(".parent");
+// let prnt = document.querySelector(".parent");
 // console.log(prnt);
 // console.log(prnt.children);
 // console.log(prnt.children.innerHtml);
 
-console.log(prnt.children[0].innerHTML);
-for(let i = 0; i< prnt.children.length ; i++){
-    console.log(prnt.children[i].innerHTML);
-}
+// console.log(prnt.children[0].innerHTML);
+// for(let i = 0; i< prnt.children.length ; i++){
+//     console.log(prnt.children[i].innerHTML);
+// }
 
-prnt.children[1].style.color = "red";
+// prnt.children[1].style.color = "red";
 
 // console.log(prnt.firstElementChild);
 // console.log(prnt.lastElementChild);
 
 
-let one = document.querySelector(".same");
-console.log(one);
-console.log(one.parentElement);
-console.log(one.nextElementSibling);
+// let one = document.querySelector(".same");
+// console.log(one);
+// console.log(one.parentElement);
+// console.log(one.nextElementSibling);
 
-console.log( prnt.childNodes);
+// console.log( prnt.childNodes);
 
 // ----------------------------new element creation --------------------
 
-let new_e = document.createElement("div");
-console.log(new_e);
-new_e.className = "new";
-new_e.id = "unique";
-new_e.setAttribute("title" , "hello")
-const addtext = document.createTextNode("Aman");
-new_e.appendChild(addtext);
+// let new_e = document.createElement("div");
+// console.log(new_e);
+// new_e.className = "new";
+// new_e.id = "unique";
+// new_e.setAttribute("title" , "hello")
+// const addtext = document.createTextNode("Aman");
+// new_e.appendChild(addtext);
 
-document.body.appendChild(addtext)
-
-
+// document.body.appendChild(addtext)
 
 
+
+
+//---------------------Edit and remove--------------------
+
+//add element 
+
+  function addname(lname){// not use 
+      let li = document.createElement("li");
+      console.log(li);
+      li.innerHTML = `${lname}`;
+      document.querySelector(".lang").appendChild(li);
+    }
+    addname("Html");
+    
+    function otmzadd(lname){
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(lname))
+        document.querySelector(".lang").appendChild(li);
+
+}
+otmzadd("css");
+// edit
+
+const sectlang = document.querySelector("li:nth-child(2)");
+const newli = document.createElement("li");
+newli.textContent = "java";
+sectlang.replaceWith(newli);
+
+//remove
+
+const lastlang = document.querySelector("li:last-child")
+lastlang.remove();
